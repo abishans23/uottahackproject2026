9170889679f6f556b8a4a89802018aac310cefe4
abishans23 <abishans23@gmail.com>
2026-01-17 15:14:54 -0500

    merge

---
+++ recipes.js
@@ -0,0 +1,44 @@
+
+        const selectedOption = document.getElementById("op");
+        const btnSelect = document.getElementById("btn");
+        const loader = document.getElementById("pender")
+        const textInput = document.getElementById("txt");
+
+        
+
+       function changeFilter(){
+            const val = selectedOption.value;
+
+
+            if(val === "ingredient"){
+                textInput.placeholder = "e.g  pickles, chocolate"
+            }
+
+            else if(val === "cuisine"){
+                textInput.placeholder = "e.g Italian or Mexican"
+            }
+
+            else if(val === "diet"){
+                textInput.placeholder = "e.g vegan or keto"
+            }
+
+            else if(val === "intolerance"){
+                textInput.placeholder = "e.g lactose or gluten"
+            }
+
+            else{
+                textInput.placeholder = "e.g  rice and curry or mango sticky rice"
+            }
+
+       }
+
+       function loadQuery(){
+            loader.innerText = "Loading....."
+            const query = document.getElementById("text").trim();
+
+       }
+       changeFilter()
+
+       selectedOption.addEventListener("change", changeFilter);
+       btnSelect.addEventListener("click", loadQuery);
+       
