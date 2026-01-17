ddda7d30e0824724f7f1817334f7c68efbba3639
abishans23 <abishans23@gmail.com>
2026-01-17 16:02:39 -0500

    branch

--- spoonicular.py
+++ spoonicular.py
@@ -8,4 +8,28 @@ app = Flask(__name__)
 
 API_KEY = os.getenv("SPOONACULAR_API_KEY")
 
+
 @app.route('/')
+def recipe():
+    return render_template("recipes.html")
+
+@app.route('/search', ["POST"])
+def search():
+    data = request.json
+    query = data.get("query")
+    filter_type = data.get("filter")
+
+    BASE_URL = "https://api.spoonacular.com/recipes/"
+    params = {"apiKey": API_KEY} 
+
+    if filter_type == "ingredients" :
+        params["ingredients"] = query
+        url = BASE_URL + "findByIngredients"
+    
+    else:
+        if filter_type == "cuisine" :
+            params["cuisine"] = query
+        
+        elif filter_type == "diet":
+            params["diet"] = query
+    
