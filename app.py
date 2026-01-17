152b08d6a435c8bb70a6b24fc2629f8d8997e5b3
Vincent Ton <vton952@gmail.com>
2026-01-17 13:12:41 -0500

    started login functionality

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
-import requests
 import os
-from dotenv import load_dotenv
-from urrlib.parse import unquote
 
 app = Flask(__name__)
 
-API_KEY = os.getenv("SPOONACULAR_API_KEY")
+API_KEY = os.getenv("API_KEY")
 
 @app.route('/home', methods=['GET'])
 def home():
     render_template("recipes.html", recipes = [], search_query = "")
 
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    return
 
-def getURL(filter):
-    print()
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
