a05ee76e2dc26d8e82b61dd46d67fef9b3d8b243
abishans23 <abishans23@gmail.com>
2026-01-17 21:22:34 -0500

    d

--- static/create.js
+++ static/create.js
@@ -2,6 +2,8 @@ const selButton = document.getElementById("btn");
 
 function saveInfo(){
 
+    const nameField = document.getElementById("name")
+    console.log(nameField.value);
 }
-
+saveInfo()
 selButton.addEventListener("onClick", saveInfo);
