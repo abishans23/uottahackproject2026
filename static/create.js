15bd351b24c3bb67487aba9e6341a4579f9bf32b
abishans23 <abishans23@gmail.com>
2026-01-17 21:42:32 -0500

    67

--- static/create.js
+++ static/create.js
@@ -4,6 +4,20 @@ function saveInfo(){
 
     const nameField = document.getElementById("name")
     console.log(nameField.value);
+
+    const ingredientField = document.getElementById("ingredients")
+    console.log(ingredientField.value);
+
+    const instructionField = document.getElementById("instructions")
+    console.log(instructionField.value)
+
+    const ingredients = ingredientField.value.split(', ')
+
+    const obj = {
+        "name":nameField.value, "ingredients":ingredients, "instructions":instructionField.value
+    }
+
+    return obj
 }
 saveInfo()
-selButton.addEventListener("onClick", saveInfo);
+selButton.addEventListener("click", saveInfo);
